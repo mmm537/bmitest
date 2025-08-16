739100edf95c4dd87c081c32a5b7eeab5a1f54cb
mmm537 <manatcc458@gmail.com>
2025-08-16 18:05:47 +0700

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,55 +1,45 @@
 import streamlit as st
-import requests
-from pathlib import Path
 
-st.title("Botnoi Voice API Demo")
-API_URL = "https://api-voice.botnoi.ai/openapi/v1/generate_audio"
-API_TOKEN = "RtR9GUvxybl8g6iJo3G9BN49Q92fcIbb"
+st.set_page_config(page_title='Body mass index : Web Application',page_icon='🦊')
+st.title('Body mass index : Web Application')
+st.balloons()
+st.snow()
 
 
-text_input = st.text_input("ข้อความที่ต้องการแปลงเป็นเสียง", "สวัสดีชาวโลก")
-speaker_id = st.text_input("Speaker ID", "1")
-generate_btn = st.button("Generate Voice")
+kg=st.number_input('น้ำหนัก (kg):')
+cm=st.number_input('ส่วนสูง (cm):')
+from gtts import gTTS
+import io
+if st.button('คำนวน'):
+  bmi = kg/((cm/100)**2)
+  tt = f'ค่า BMI ของคุณคือ {bmi:.2f}'
+  if bmi < 18.5:
+    st.info(tt)
+    st.image('b1.png')
+    word = "น้ำหนักน้อยกว่าปกติ"
 
-if generate_btn:
-    payload = {
-        "text": text_input,
-        "speaker": speaker_id,
-        "volume": 1,
-        "speed": 1,
-        "type_media": "mp3",
-        "save_file": "true",
-        "language": "th",
-        "page": "user"
-    }
+  elif bmi < 24.9:
+    st.success(tt)
+    st.image('b2.png')
+    word = "น้ำหนักปกติ"
 
-    headers = {
-        "accept": "application/json",
-        "Content-Type": "application/json",
-        "botnoi-token": API_TOKEN
-    }
+  elif bmi < 29.9:
+    st.warning(tt)
+    st.image('b3.png')
+    word = "น้ำหนักเกิน"
 
-    try:
-        res = requests.post(API_URL, json=payload, headers=headers, timeout=30)
-        res.raise_for_status()
-        data = res.json()
-        st.write("API Response:", data)
+  elif bmi < 39.9:
+    st.warning(tt)
+    st.image('b4.png')
+    word = "โรคอ้วน"
 
-        # ดึง URL ไฟล์เสียง
-        audio_url = (
-            data.get("url")
-            or data.get("audio_url")
-            or (data.get("data") or {}).get("url")
-        )
+  else:
+    st.warning(tt)
+    st.image('b5.png')
+    word = "โรคอ้วนอันตราย"
 
-        if audio_url:
-            audio_bytes = requests.get(audio_url, timeout=30).content
-            out_path = Path("botnoi_voice.mp3")
-            out_path.write_bytes(audio_bytes)
-            st.success(f"✅ บันทึกเสียงเรียบร้อย → {out_path.resolve()}")
-            st.audio(audio_bytes, format="audio/mp3")
-        else:
-            st.error("ไม่พบลิงก์ไฟล์เสียงใน response")
-
-    except Exception as e:
-        st.error(f"เกิดข้อผิดพลาด: {e}")
+  tts = gTTS(text=word, lang='th')
+  mp3_fp = io.BytesIO()
+  tts.write_to_fp(mp3_fp)
+  mp3_fp.seek(0)
+  st.audio(mp3_fp, format="audio/mp3")
